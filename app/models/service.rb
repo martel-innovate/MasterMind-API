0600417e23538d433901eb76314ada7cf008535a
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-09-12 16:20:28 +0200

    Fixed validation error with service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,6 +4,7 @@ class Service < ApplicationRecord
   belongs_to :cluster
   belongs_to :service_type
 
-  validates_presence_of :configuration, :status, :managed, :endpoint, :docker_service_id, :latitude, :longitude
+  validates_presence_of :configuration, :status, :endpoint, :docker_service_id, :latitude, :longitude
+  validates_inclusion_of :managed, :in => [true, false]
 
 end
