bde37ea8cf77d21d440ce03922f9290fd890167e
Omer Ozdemir <omer.ozdemir@atos.net>
2017-11-20 23:00:04 +0100

    removed unused method from authorize request

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :authorize_request, only: [:authenticate, :login]
+  skip_before_action :authorize_request, only: [:authenticate]
 
   swagger_controller :authentication, "Actors Authentication"
 
