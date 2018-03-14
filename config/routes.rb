5b24a05703d3c8e4d239a06f5eebc77338ff3187
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-03-14 15:40:06 +0100

    Removed unusued methods to deploy services via command line docker client

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,8 @@ Rails.application.routes.draw do
     end
 
     # Cluster routes for deploying stacks, removing stacks, getting info
-    get '/projects/:project_id/clusters/:id/info', to: 'clusters#info'
-    get '/projects/:project_id/clusters/:id/version', to: 'clusters#version'
     get '/projects/:project_id/clusters/:id/getstack', to: 'clusters#getStack'
     get '/projects/:project_id/clusters/:id/deploy', to: 'clusters#deploy'
-    get '/projects/:project_id/clusters/:id/deploywithdockerclient', to: 'clusters#deployWithDockerClient'
     get '/projects/:project_id/clusters/:id/removestack', to: 'clusters#removeStack'
 
     # NGSI Subs routes for registering, deleting, toggling subs
