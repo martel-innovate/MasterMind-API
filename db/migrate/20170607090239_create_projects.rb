2964e66054543cc97db42c771e63dd10c3c56d0c
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-06-19 10:08:33 +0200

    fixes to database types

--- db/migrate/20170607090239_create_projects.rb
+++ db/migrate/20170607090239_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
--- db/migrate/20170616133923_create_clusters.rb
+++ db/migrate/20170616133923_create_clusters.rb
@@ -2,11 +2,11 @@ class CreateClusters < ActiveRecord::Migration[5.1]
   def change
     create_table :clusters do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.string :endpoint
-      t.string :cert
-      t.string :key
-      t.string :ca
+      t.text :cert
+      t.text :key
+      t.text :ca
       t.references :project, foreign_key: true
 
       t.timestamps
